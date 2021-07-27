32429951bf29b9cc8477823f524a193b35106d1e
John Letey <johnletey@gmail.com>
2021-07-27 17:18:16 +0100

    feat: add gifs to reply

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ config();
 // Import dependencies.
 import Arweave from "arweave";
 import { JWKInterface } from "arweave/node/lib/wallet";
+import fs from "fs";
 import { MongoClient } from "mongodb";
 import needle from "needle";
 import { interactWrite } from "smartweave";
@@ -38,6 +39,37 @@ const fetchTweet = async (id: string): Promise<string | undefined> => {
   }
 };
 
+const selectGifAndUpload = async (): Promise<string> => {
+  const files = fs.readdirSync("gifs");
+  const file = files[Math.floor(Math.random() * files.length)];
+
+  const user = new Twitter({
+    subdomain: "upload",
+    consumer_key: process.env.CONSUMER_KEY?.toString()!,
+    consumer_secret: process.env.CONSUMER_SECRET?.toString()!,
+    access_token_key: process.env.ACCESS_KEY?.toString()!,
+    access_token_secret: process.env.ACCESS_SECRET?.toString()!,
+  });
+
+  const { media_id_string } = await user.post("media/upload", {
+    command: "INIT",
+    total_bytes: fs.statSync(`gifs/${file}`).size,
+    media_type: "image/gif",
+  });
+  await user.post("media/upload", {
+    command: "APPEND",
+    media_id: media_id_string,
+    media_data: Buffer.from(fs.readFileSync(`gifs/${file}`)).toString("base64"),
+    segment_index: 0,
+  });
+  await user.post("media/upload", {
+    command: "FINALIZE",
+    media_id: media_id_string,
+  });
+
+  return media_id_string;
+};
+
 const postTweet = async (text: string, id: string): Promise<string> => {
   const user = new Twitter({
     consumer_key: process.env.CONSUMER_KEY?.toString()!,
@@ -50,6 +82,7 @@ const postTweet = async (text: string, id: string): Promise<string> => {
     status: text,
     in_reply_to_status_id: id,
     auto_populate_reply_metadata: true,
+    media_ids: await selectGifAndUpload(),
   });
 
   return res.id_str;
